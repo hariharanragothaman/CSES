00bcd601f7313aab78aba9574575c3473ac99c74
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-06-18 23:23:48 -0400

    Even KMP is TLE'ing?

---
+++ 08_string_algorithms/finding_patterns_kmp.py
@@ -0,0 +1,51 @@
+def partial(s):
+    g, pi = 0, [0] * len(s)
+    for i in range(1, len(s)):
+        while g and (s[g] != s[i]):
+            g = pi[g - 1]
+        pi[i] = g = g + (s[g] == s[i])
+
+    return pi
+
+
+def match(s, pat):
+    pi = partial(pat)
+
+    g, idx = 0, []
+    for i in range(len(s)):
+        while g and pat[g] != s[i]:
+            g = pi[g - 1]
+        g += pat[g] == s[i]
+        if g == len(pi):
+            idx.append(i + 1 - g)
+            g = pi[g - 1]
+
+    return idx
+
+
+def string_find(s, pat):
+    pi = partial(pat)
+
+    g = 0
+    for i in range(len(s)):
+        while g and pat[g] != s[i]:
+            g = pi[g - 1]
+        g += pat[g] == s[i]
+        if g == len(pi):
+            return True
+
+    return False
+
+
+if __name__ == '__main__':
+    text = input()
+    k = int(input())
+    i = 0
+    while i < k:
+        pattern = input()
+        result = string_find(text, pattern)
+        if result:
+            print('YES')
+        else:
+            print('NO')
+        i += 1
