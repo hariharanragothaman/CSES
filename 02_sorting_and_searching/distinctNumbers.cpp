6ad03d38b1eece324d1659786c9ed1bb39a48ef6
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-31 12:52:53 -0400

    Adding some CSES solutions sets

---
+++ 02_sorting_and_searching/distinctNumbers.cpp
@@ -0,0 +1,101 @@
+/* BEGIN HEADER FILES AND OTHER OPTIZATIONS */
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+// For Hash
+#include <functional>
+
+// For all useful constants
+#include <climits>
+
+// Data-Structures and related stuff.
+#include <map>
+#include <set>
+#include <stack>
+#include <random>
+#include <deque>
+#include <queue>   // Includes Priority Queue
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+
+// Some basic typedef's for comfort
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// #defines for bounds comfort
+#define double long double
+
+// #defines for comfort
+#define f first
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+// Binary Search comforts
+#define lb lower_bound
+#define ub upper_bound
+
+
+// begin deepsigh
+//#define min(x,y) ({ __typeof__(x) __var0 = x; __typeof__(y) __var1 = y; __var0 < __var1 ? __var0 : __var1; })
+//#define max(x,y) ({ __typeof__(x) __var0 = x; __typeof__(y) __var1 = y; __var0 < __var1 ? __var1 : __var0; })
+//static const int ENABLEFASTIO = []() { std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr); return 0; }();
+// end deepsigh
+
+// BEGIN NO SAD
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+// END NO SAD
+
+/* END HEADER FILES AND OTHER OPTIZATIONS */
+
+/*
+Some General Advice
+
+REMEMBER CLEAR GLOBAL STATE
+REMEMBER READ THE PROBLEM STATEMENT AND DON'T SOLVE A DIFFERENT PROBLEM
+remember hidden T factor of 1e2
+read the bounds for stupid cases
+did you restart your editor
+pushing back vectors is garbage, pre-initialize them
+function calls are not free
+*/
+
+int main()
+{
+    int  n;
+    cin >> n;
+    vi A(n);
+    for(int i=0; i<n; i++)
+        cin >> A[i];
+    map<int, int> hmap;
+
+    for(int i=0; i<n; i++)
+    {
+        hmap[A[i]] += 1;
+    }
+
+    cout << hmap.size();
+    return 0;
+
+}
---
+++ 04_graph_algorithms/06_round_trip.py
@@ -0,0 +1,42 @@
+import sys, math, cmath, time, collections
+from collections import deque, Counter, OrderedDict, defaultdict
+from heapq import nsmallest, nlargest, heapify, heappop, heappush, heapreplace
+from math import ceil, floor, log, log2, sqrt, gcd, factorial, pow, pi
+from bisect import bisect_left, bisect_right
+
+start_time = time.time()
+
+
+def solve(a, b):
+    """
+    To print a cycle in the graph
+    """
+    pass
+
+
+def main():
+    """
+    Main function dedicated to get the I/P
+    a, b = map(int, input().split())
+    solve(a, b)
+    """
+    a, b = map(int, input().split())
+    solve(a, b)
+
+
+if __name__ == "__main__":
+    LOCAL = False
+
+    # If it's Local - Get I/P from file
+    if LOCAL:
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    testcases = int(input())
+    for i in range(testcases):
+        main()
+
+    # If it's local - Print this O/P
+    if LOCAL:
+        print(f"Time Elapsed: {time.time() - start_time} seconds")
+        sys.stdout.close()
