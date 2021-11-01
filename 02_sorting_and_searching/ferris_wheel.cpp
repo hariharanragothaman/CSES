388744b1f41442dd24772139dddfd2466930e79d
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-11-01 02:15:35 -0400

    Ferris wheel solution, be very clear on what chunk of memory you want and you are inputting

--- 02_sorting_and_searching/ferris_wheel.cpp
+++ 02_sorting_and_searching/ferris_wheel.cpp
@@ -1,95 +1,56 @@
 //
 // Created by Hariharan Ragothaman on 10/31/21.
 //
+#include<bits/stdc++.h>
+using namespace std;
 
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("unroll-loops")
 #pragma GCC optimize("inline")
-
-// GODSPEED
 #define ENABLEFASTIO() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
-#include<bits/stdc++.h>
-using namespace std;
 
-// Some basic typedef's for comfort
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<pii> vii;
+const int maxn = 2e5+10;
+bool have_gondola_yet[maxn];
 
-// #defines for bounds comfort
-#define double long double
-
-// #defines for comfort
-#define f first
-#define s second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-
-// Binary Search comforts
-#define lb lower_bound
-#define ub upper_bound
-
-// Debugging related comforts
-#define debug(x) cout << #x << " is " << x << endl
-#define  mset(a,x)      memset(a,x,sizeof(a))
-#define  debv(a)        for(auto it: a)cout<<it<<" ";newl;
-#define  deb1(a)        cout<<a<<"\n";
-#define  deb2(a,b)      cout<<a<<" "<<b<<"\n";
-#define  deb3(a,b,c)    cout<<a<<" "<<b<<" "<<c<<"\n";
-#define  deb4(a,b,c,d)  cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";
-
-// Traversal related Comforts
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-#define  uniq(a)  a.resize(unique(a.begin(), a.end()) - a.begin());
-
-// Other Customizations
-#define present(container, element) (container.find(element) != container.end())
-#define cpresent(container, element) (find(all(container), element) != container.end())
-bool sortBySecond(const pair<int,int> &a, const pair<int,int> &b)
+void solve()
 {
-    return (a.second < b.second);
-}
-
-int main()
-{
-    ENABLEFASTIO();
-
     int children, maximumWeight;
     cin >> children >> maximumWeight;
-    vector<int> arr;
-
-    int weight;
-
-    while(children--)
-    {
-        cin >> weight;
-        arr.emplace_back(weight);
-    }
+    vector<int> arr(children);
+    for(int i=0; i < children; i++) cin >> arr[i];
 
     sort(arr.begin(), arr.end());
 
-    int result = 0;
-    int total = 0;
+    int count = 0;
+    int left = 0;
+    int right = children - 1;
 
-    for(auto c: arr)
+    while (left < right)
     {
-        if(total + c <= maximumWeight)
+        if(arr[left] + arr[right] <= maximumWeight)
         {
-            total += c;
+            count++;
+            have_gondola_yet[left] = have_gondola_yet[right] = true;
+            right--;
+            left++;
         }
         else
         {
-            total = c;
-            result++;
+            right--;
         }
     }
-    cout << result+1;
+
+    for(int i=0; i < children; i++)
+    {
+        count += have_gondola_yet[i] == false;
+    }
+
+    cout << count << endl;
+}
+
+int main(){
+    ENABLEFASTIO();
+    solve();
     return 0;
 }
