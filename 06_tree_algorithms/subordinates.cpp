e3f5bb710cac86ca1be47e7a87e6edc1470302c3
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-11-02 23:48:08 -0400

    Solving problems in C++ yaaay

---
+++ 06_tree_algorithms/subordinates.cpp
@@ -0,0 +1,75 @@
+//
+// Created by Hariharan Ragothaman on 11/2/21.
+//
+
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve( map<int, vector<int>>& graph, int n)
+{
+
+    // Starting Post order traversal
+    deque<int> q;
+    q.emplace_back(1);
+    int node;
+    vector<int> res_tmp;
+    vector<int> postorder;
+
+    while(q.size() > 0)
+    {
+        node = q.back();
+        q.pop_back();
+        res_tmp.push_back(node);
+        if(graph[node].size() > 0)
+        {
+            for(auto c: graph[node])
+                q.emplace_back(c);
+        }
+    }
+    while(res_tmp.size() > 0)
+    {
+        postorder.emplace_back(res_tmp.back());
+        res_tmp.pop_back();
+    }
+    /* End of post order traversal */
+
+    vector<int> childrenCount(n+1);
+    for(auto ele: postorder)
+    {
+        if(graph.find(ele) == graph.end())
+        {
+            childrenCount[ele] = 0;
+        }
+        else
+        {
+            int count = 0;
+            count += graph[ele].size();
+            for(auto children: graph[ele])
+                count += childrenCount[children];
+            childrenCount[ele] = count;
+        }
+    }
+
+    for(int i=1; i<=n; i++)
+        cout << childrenCount[i] << " ";
+    cout << endl;
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n+1);
+    for (int i=2; i<arr.size(); i++)
+        cin >> arr[i];
+
+    map<int, vector<int>> graph;
+    graph[1] = {};
+
+    for(int j=2; j < arr.size(); j++)
+        graph[arr[j]].emplace_back(j);
+
+    solve(graph, n);
+
+}
---
+++ 08_string_algorithms/word_combinations.cpp
@@ -0,0 +1,122 @@
+//
+// Created by Hariharan Ragothaman on 11/2/21.
+//
+
+
+#include "bits/stdc++.h"
+using namespace std;
+
+/*
+ *  Given a string, and K words
+ *  in how many ways, can you create the string using the words?
+ */
+
+class TrieNode
+{
+public:
+    bool isLeaf;
+    TrieNode* children[26];
+    TrieNode()
+    {
+        isLeaf = false;
+        for(int i=0; i<26; i++) children[i] = NULL;
+    }
+};
+
+class Trie
+{
+public:
+    TrieNode* root;
+    Trie()
+    {
+        root = new TrieNode;
+    }
+
+    void insert(string word)
+    {
+        auto n = word.size();
+        TrieNode* current = root;
+        for(int i=0; i<n; i++)
+        {
+            int val = word[i] - 'a';
+            if(current->children[val] == NULL)
+            {
+                current->children[val] = new TrieNode();
+            }
+            current = current->children[val];
+        }
+        current->isLeaf = true;
+    }
+
+    bool search(string word)
+    {
+        auto n =word.size();
+        TrieNode* current = root;
+        for(int i=0; i<n; i++)
+        {
+            int val = word[i] - 'a';
+            if(current->children == NULL)
+            {
+                return false;
+            }
+            current = current->children[val];
+        }
+        return current->isLeaf;
+    }
+
+    bool startWith(string prefix)
+    {
+        auto n = prefix.size();
+        TrieNode* current = root;
+        for(int i=0; i<n; i++)
+        {
+            int val = prefix[i] - 'a';
+            if(current->children == NULL)
+            {
+                return false;
+            }
+            current = current->children[val];
+        }
+        return true;
+    }
+
+};
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    int T;
+    cin >> T;
+    string word;
+    Trie* obj = new Trie();
+    while(T--)
+    {
+        cin >> word;
+        obj->insert(word);
+
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
