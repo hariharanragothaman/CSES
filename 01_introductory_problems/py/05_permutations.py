08494ade1b4aaf673054f30ed9bd5877744c5f4f
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-04-24 17:19:53 +0530

    CSES-Problem5 Solution

--- 01_introductory_problems/py/05_permutations.py
+++ 01_introductory_problems/py/05_permutations.py
@@ -1,22 +1,64 @@
-"""
-Create permutation, such that consecutive difference is greater than 1
-"""
+import os
+import sys
+import time
 
-n = int(input())
 
-if n == 1:
-    print(1)
-elif n == 4:
-    res = [2, 4, 1, 3]
-    print(*(res))
-elif n < 5:
-    print("NO SOLUTION")
-elif n >= 5:
-    # Print all odd numbers in decreasing order, then all even numbers in decreasing order
-    even, odd = [], []
-    for i in range(n, 0, -1):
-        if i & 1:
-            odd.append(i)
-        else:
-            even.append(i)
-    print(*(odd + even))
+def solve():
+    n = int(input())
+    if n == 1:
+        print(1)
+        return
+    elif n == 2 or n == 3:
+        print("NO SOLUTION")
+        return
+    elif n == 4:
+        print("2 4 1 3")
+        return
+    else:
+        tmp = n
+        # Print all odd numbers first, followed by even numbers
+        while n > 0:
+            print(n, end=" ")
+            n = n - 2
+        tmp = tmp - 1
+        while tmp > 0:
+            print(tmp, end=" ")
+            tmp = tmp - 2
+
+
+def main() -> None:
+    if os.path.exists("data.in"):
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    start_time = time.time()
+
+    solve()
+    debug('-')
+
+    if os.path.exists("data.in"):
+        print(f"Time Elapsed: {time.time() - start_time} seconds")
+        sys.stdout.close()
+
+
+# region debug
+
+def input_as_array() -> list[int]:
+    return list(map(int, input().split()))
+
+
+def debug(char) -> None:
+    if os.path.exists("data.in"):
+        print(char * 25)
+
+
+def debug2(value) -> None:
+    if os.path.exists("data.in"):
+        print(value)
+
+
+# endregion
+
+
+if __name__ == "__main__":
+    main()
