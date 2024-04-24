869b79383da9d68c26312a222cffb50f2f6088c3
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-04-24 16:51:57 +0530

    CSES-Problem3 Solution

--- 01_introductory_problems/py/03_repititions.py
+++ 01_introductory_problems/py/03_repititions.py
@@ -1,17 +1,59 @@
-s = input()
-n = len(s)
+import os
+import sys
+import time
 
-count = 1
-prev = s[0]
-temp = []
 
-i = 0
-while i < n - 1:
-    if s[i] == s[i + 1]:
-        count += 1
-    else:
-        temp.append(count)
-        count = 1
-    i += 1
-temp.append(count)
-print(max(temp))
+def solve():
+    S = input()
+    cnt = 1
+    n = len(S)
+    max_cnt = 1
+
+    for i in range(1, n):
+        if S[i] == S[i-1]:
+            cnt += 1
+            max_cnt = max(max_cnt, cnt)
+        else:
+            cnt = 1
+
+    print(max_cnt)
+
+
+
+
+def main() -> None:
+    if os.path.exists("data.in"):
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    start_time = time.time()
+
+    solve()
+    debug('-')
+
+    if os.path.exists("data.in"):
+        print(f"Time Elapsed: {time.time() - start_time} seconds")
+        sys.stdout.close()
+
+
+# region debug
+
+def input_as_array() -> list[int]:
+    return list(map(int, input().split()))
+
+
+def debug(char) -> None:
+    if os.path.exists("data.in"):
+        print(char * 25)
+
+
+def debug2(value) -> None:
+    if os.path.exists("data.in"):
+        print(value)
+
+
+# endregion
+
+
+if __name__ == "__main__":
+    main()
