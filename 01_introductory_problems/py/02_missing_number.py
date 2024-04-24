6f12be5150bb79f1d2ab303217f67bf31b8c5cd5
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-04-24 16:44:32 +0530

    CSES-Problem2 Solution

--- 01_introductory_problems/py/02_missing_number.py
+++ 01_introductory_problems/py/02_missing_number.py
@@ -1,3 +1,49 @@
-n = int(input())
-arr = list(map(int, input().split()))
-print(((n * (n + 1) // 2) - sum(arr)))
+import os
+import sys
+import time
+
+
+def solve():
+    n = int(input())
+    A = input_as_array()
+    total_sum = (n * (n+1)) >> 1
+    array_sum = sum(A)
+    print(total_sum - array_sum)
+
+
+def main() -> None:
+    if os.path.exists("data.in"):
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    start_time = time.time()
+
+    solve()
+    debug('-')
+
+    if os.path.exists("data.in"):
+        print(f"Time Elapsed: {time.time() - start_time} seconds")
+        sys.stdout.close()
+
+
+# region debug
+
+def input_as_array() -> list[int]:
+    return list(map(int, input().split()))
+
+
+def debug(char) -> None:
+    if os.path.exists("data.in"):
+        print(char * 25)
+
+
+def debug2(value) -> None:
+    if os.path.exists("data.in"):
+        print(value)
+
+
+# endregion
+
+
+if __name__ == "__main__":
+    main()
