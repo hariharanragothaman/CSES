cb4067ac53b1497600d66421f1becf2bb998f1c4
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2026-06-07 21:09:09 -0500

    Solving CSES

--- 02_sorting_and_searching/02_apartments.cpp
+++ 02_sorting_and_searching/02_apartments.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 
 void solve() {
-    // TODO: not solved
+    int n, m, k; 
+    cin >> n >> m >> k;
+    vector<int> desired_apt_size(n);
+    vector<int> apt_size(m);
+    for(int i=0; i<n; i++) {
+        cin >> desired_apt_size[i];
+    }
+    for(int i=0; i<m; i++) {
+        cin >> apt_size[i];
+    }
+    sort(desired_apt_size.begin(), desired_apt_size.end());
+    sort(apt_size.begin(), apt_size.end());
+    int count = 0; 
+
+    int i = 0, j = 0; 
+    while(i < n && j < m) {
+        if(apt_size[j] < desired_apt_size[i] - k) 
+            j++; 
+        else if(apt_size[j] > desired_apt_size[i] + k)
+            i++; 
+        else {
+            count++; 
+            i++; 
+            j++; 
+        }
+    }
+    cout << count << endl;
 }
 
 int main() {
